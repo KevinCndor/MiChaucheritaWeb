c42205d71cf134505b73c955ad793270ad59ff8b
KevinCndor <kevin.condor@epn.edu.ec>
2023-08-25 21:52:06 -0500

    Update JPADAOFactory.

--- src/main/java/modelo/jpa/JPADAOFactory.java
+++ src/main/java/modelo/jpa/JPADAOFactory.java
@@ -1,10 +1,12 @@
 package modelo.jpa;
 
+import modelo.dao.CategoriaDAO;
 import modelo.dao.CuentaDAO;
 import modelo.dao.DAOFactory;
 import modelo.dao.EgresoDAO;
 import modelo.dao.IngresoDAO;
 import modelo.dao.MovimientoDAO;
+import modelo.dao.SubcategoriaDAO;
 import modelo.dao.TransferenciaDAO;
 import modelo.dao.UsuarioDAO;
 
@@ -38,6 +40,16 @@ public class JPADAOFactory extends DAOFactory{
 	@Override
 	public TransferenciaDAO getTransferenciaDAO() {
 		return new JPATransferenciaDAO();
+	}
+
+	@Override
+	public CategoriaDAO getCategoriaDAO() {
+		return new JPACategoriaDAO();
+	}
+
+	@Override
+	public SubcategoriaDAO getSubcategoriaDAO() {
+		return new JPASubcategoriaDAO();
 	}
 
 }
