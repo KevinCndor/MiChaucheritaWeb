44514629232053a431c0ad54a62e7aeb47791d0f
Rafa Robalino <rafaela.robalino@epn.edu.ec>
2023-09-03 15:09:43 -0500

    Elimina movimiento en movimiento.jsp

--- src/main/java/controlador/movimiento/MovimientoController.java
+++ src/main/java/controlador/movimiento/MovimientoController.java
@@ -117,7 +117,7 @@ public class MovimientoController extends HttpServlet {
 		//1. Obtener datos que me envian en la solicitud
 		Usuario usuario = getSession(request);
 		
-		String descripcion = request.getParameter("descripcion");
+		String descripcion = request.getParameter("monto");
 		
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String fechaFormateada = LocalDate.now().format(formato);
